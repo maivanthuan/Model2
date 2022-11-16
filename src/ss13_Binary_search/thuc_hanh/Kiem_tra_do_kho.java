d16e54e604efd2b5d968d6c6d182a23f7f3e5cf3
maivanthuan <thuan31102003@gmail.com>
2022-11-16 18:56:00 +0700

    dá

--- src/ss13_Binary_search/thuc_hanh/Kiem_tra_do_kho.java
+++ src/ss13_Binary_search/thuc_hanh/Kiem_tra_do_kho.java
@@ -1,2 +1,25 @@
-package ss13_Binary_search.thuc_hanh;public class Kiem_tra_do_kho {
+package ss13_Binary_search.thuc_hanh;
+
+import java.util.Scanner;
+
+public class Kiem_tra_do_kho {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input=scanner.nextLine();
+        int[] frequentchar=new int[255];
+        for (int i=0;i<input.length();i++){
+            int ascii=input.charAt(i);
+            frequentchar[ascii]+=1;
+        }
+        int max=0;
+            char charater=(char) 255;
+            for (int j=0;j<255;j++){
+                if (frequentchar[j]>max){
+                    max=frequentchar[j];
+                    charater=(char) j;
+                }
+            }
+        System.out.println("The most appearing letter is '" + charater + "' with a frequency of " + max + " times");
+    }
 }
