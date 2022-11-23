ca71ee901cd9c6082f819f54ced9c2b83da45891
maivanthuan <thuan31102003@gmail.com>
2022-11-23 19:24:42 +0700

    dsa

--- src/ss15_Exception/bai_tap/exception.java
+++ src/ss15_Exception/bai_tap/exception.java
@@ -1,2 +1,7 @@
-package ss15_Exception.bai_tap;public class exception {
+package ss15_Exception.bai_tap;
+
+public class exception extends Exception {
+    public exception(String str){
+        super(str);
+    }
 }
